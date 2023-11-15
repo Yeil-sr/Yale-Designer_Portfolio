0c32c9910fea3c1af5b8ff0155474804d1a18478
Yeil-sr <Blackfootout@gmail.com>
2023-11-15 21:37:50 -0200

    arquivos adicionados

---
+++ index.js
@@ -0,0 +1,56 @@
+﻿require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const porta = process.env.PORT || 443 ; 
+const path = require('path');
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
+const public = path.join(__dirname, "public");
+
+app.use(express.static(public));
+app.use(bodyParser.urlencoded({ extended: true })); 
+
+
+app.post('/envio', (req, res) => {
+    const { nome, emailCliente, fone, celular, assunto, preferencia, mensagem } = req.body;
+
+    const mailOptions = {
+        from: emailCliente, 
+        to: process.env.SMTP_USER, 
+        subject: 'Formulário de Contato - ' + assunto,
+        text: `
+            Nome: ${nome}
+            E-mail: ${emailCliente} 
+            Telefone: ${fone}
+            Celular: ${celular}
+            Assunto: ${assunto}
+            Preferência de Contato: ${preferencia}
+            
+            Mensagem:
+            ${mensagem}
+        `
+    };
+
+    const transporter = nodemailer.createTransport({
+        service: 'Gmail', 
+        auth: {
+            user: process.env.SMTP_USER,
+            pass: process.env.SMTP_PASS
+        }
+    });
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.log(error);
+            res.status(500).send('Erro ao enviar o e-mail');
+        } else {
+            console.log('E-mail enviado: ' + info.response);
+            res.send('E-mail enviado com sucesso!');
+        }
+    });
+});
+
+app.listen(porta, () => {
+    console.log(`Servidor Express rodando na porta ${porta}`);
+});
---
+++ public/funções/main.js
@@ -0,0 +1,103 @@
+function slide3d() {
+  const screenWidth = window.innerWidth;
+  const screenHeight = window.innerHeight;
+
+  const imagens = ["public/img/load.gif", 'public/img/infantil.png', 'public/img/d27.png', 'public/img/h1.png', 'public/img/d11.png', 'public/design/gta1.png', 'public/img/otto.png', 'public/design/MaiDefinitive.png', 'public/img/lobo.png', 'public/design/my.png', 'public/design/un.png', 'public/design/fg.png', 'public/design/nago.png', "public/img/load.gif"];
+  
+  let fotoClasses = ['foto1', 'foto2', 'foto3', 'foto4', 'foto5'];
+
+  // Índice atual para rastrear a posição atual do slide
+  let indiceAtual = 0;
+  
+  const fotos = document.querySelectorAll('.foto');
+  const prev = document.querySelector('.prev');
+  prev.addEventListener('click', () => {
+    prev.classList.toggle('active');
+    const firstClass = fotoClasses.shift();
+    fotoClasses.push(firstClass);
+    fotos.forEach((foto, index) => {
+      foto.classList.remove(fotoClasses[(index + fotoClasses.length - 1) % fotoClasses.length]);
+      foto.classList.add(fotoClasses[index]);
+    });
+
+    indiceAtual = (indiceAtual - 1 + imagens.length) % imagens.length;
+    fotos.forEach((foto, index) => {
+      const nextIndex = (index + 1) % imagens.length;
+      foto.src = imagens[(indiceAtual + nextIndex) % imagens.length];
+    });
+  });
+
+  const next = document.querySelector('.next');
+  next.addEventListener('click', () => {
+    next.classList.toggle('active');
+    const lastClass = fotoClasses.pop();
+    fotoClasses.unshift(lastClass);
+    fotos.forEach((foto, index) => {
+      foto.classList.remove(fotoClasses[(index + 1) % fotoClasses.length]);
+      foto.classList.add(fotoClasses[index]);
+    });
+
+    indiceAtual = (indiceAtual + 1) % imagens.length;
+    fotos.forEach((foto, index) => {
+      const nextIndex = (index + 1) % imagens.length;
+      foto.src = imagens[(indiceAtual + nextIndex) % imagens.length];
+    });
+  });
+
+  const image = document.querySelectorAll('.ft');
+  image.forEach((ft, index) => {
+    ft.addEventListener('click', () => {
+      const sl = document.querySelector('#slide');
+      sl.classList.add('active');
+      const clickedImgSrc = ft.getAttribute('src');
+      const imgIndex = imagens.findIndex(imgSrc => imgSrc === clickedImgSrc);
+      indiceAtual = imgIndex;
+      document.querySelector('#slide img').src = clickedImgSrc;
+      fotos.forEach((foto, index) => {
+        foto.src = imagens[(indiceAtual + index) % imagens.length];
+      });
+    });
+  });
+
+  const mais = document.querySelector('.mais');
+  mais.addEventListener('click', () => {
+    const sl = document.querySelector('#slide');
+    sl.classList.toggle('active');
+  });
+
+  // Guardar as classes originais das imagens
+  const classesOriginais = [...fotoClasses];
+  const close = document.querySelector('#close');
+  close.addEventListener('click', () => {
+    fotos.forEach((foto, index) => {
+      foto.classList.remove(fotoClasses[index]);
+      foto.classList.add(classesOriginais[index]);
+    });
+    fotoClasses = [...classesOriginais];
+    indiceAtual = 0;
+    fotos.forEach((foto, index) => {
+      foto.src = imagens[(indiceAtual + index) % imagens.length];
+    });
+    // Fechar o slide removendo a classe 'active'
+    const sl = document.querySelector('#slide');
+    sl.classList.remove('active');
+    prev.classList.remove('active');
+    next.classList.remove('active');
+  });
+}
+
+window.addEventListener('load', slide3d);
+window.addEventListener('resize', slide3d);
+     //opera
+      const opBrowser = document.querySelector('.opBrowser')
+      opBrowser.addEventListener('click', () => {
+      const nave = document.querySelector('.nave');
+      nave.classList.toggle('active');
+  })
+//menuMobile
+   const btn = document.querySelector('.btn')
+   btn.addEventListener('click', ()=> {
+    const nave = document.querySelector('#menuL')
+    menuL.classList.toggle('active');
+    
+   })  
