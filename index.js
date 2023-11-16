d439d2afcaba5d8dda302ab7b9d53dc9db1f6665
Yeil-sr <Blackfootout@gmail.com>
2023-11-16 01:19:53 -0200

    serve configurado

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const app = express();
-const porta = process.env.PORT || 443 ; 
+const porta = process.env.PORT || 3000; 
 const path = require('path');
 const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser');
@@ -11,6 +11,9 @@ const public = path.join(__dirname, "public");
 app.use(express.static(public));
 app.use(bodyParser.urlencoded({ extended: true })); 
 
+app.get('/home', (req, res) => {
+    res.sendFile(path.join(__dirname, "sites/index.html"));
+});
 
 app.post('/envio', (req, res) => {
     const { nome, emailCliente, fone, celular, assunto, preferencia, mensagem } = req.body;
@@ -33,20 +36,38 @@ app.post('/envio', (req, res) => {
     };
 
     const transporter = nodemailer.createTransport({
-        service: 'Gmail', 
+        host: process.env.SMTP_HOST,
+        port: process.env.SMTP_PORT,
+        secure: false, // Não use SSL
+        requireTLS: true, // Use STARTTLS
         auth: {
             user: process.env.SMTP_USER,
             pass: process.env.SMTP_PASS
+        },
+        tls: {
+            rejectUnauthorized: false
         }
     });
-
-    transporter.sendMail(mailOptions, (error, info) => {
+            
+    // Verificar se as variáveis de ambiente estão configuradas corretamente
+    console.log('SMTP Host:', process.env.SMTP_HOST);
+    console.log('SMTP Port:', process.env.SMTP_PORT);
+    console.log('SMTP User:', process.env.SMTP_USER);
+    transporter.verify(function(error, success) {
         if (error) {
             console.log(error);
-            res.status(500).send('Erro ao enviar o e-mail');
+            res.status(500).send('Erro ao verificar a conexão com o servidor de e-mail: ' + error.message);
         } else {
-            console.log('E-mail enviado: ' + info.response);
-            res.send('E-mail enviado com sucesso!');
+            console.log('Conexão com o servidor de e-mail bem-sucedida');
+            transporter.sendMail(mailOptions, (error, info) => {
+                if (error) {
+                    console.log(error);
+                    res.status(500).send('Erro ao enviar o e-mail: ' + error.message);
+                } else {
+                    console.log('E-mail enviado: ' + info.response);
+                    res.send('E-mail enviado com sucesso!');
+                }
+            });
         }
     });
 });
